fc32414b9cba08d7741e92a8048a7fee8d3c4708
aerum-28 <lucasbout072@gmail.com>
2026-06-27 15:58:30 +0200

    Add files via upload

--- spm-deploy/start.sh
+++ spm-deploy/start.sh
@@ -3,13 +3,12 @@ set -e
 
 # Start Node.js API in background
 cd /app/api
-npm install
-npm start &
+yarn start &
 API_PID=$!
 
 # Wait for API to be ready
 sleep 3
 
-# Start Caddy in foreground (exec remplace le processus shell)
+# Start Caddy in foreground
 cd /app
 exec caddy run --config Caddyfile
